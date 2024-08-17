f0003dc59946bb67187d3ed2e9315b27aac2a27b
zmr233 <zmr_233@outlook.com>
2024-08-17 17:25:21 +0800

    chore: Update __predeps__ installation script and tmux configuration file

--- regfiles/tmux.sh
+++ regfiles/tmux.sh
@@ -39,6 +39,7 @@ add_configMap config_map
 
 # 配置文件 ./.tmux.conf 
 cat << 'EOF' >> $TEMP/./.tmux.conf
+
 # 设置鼠标支持
 set -g mouse on
 
@@ -59,6 +60,19 @@ bind l select-pane -R
 # 这样，你就可以在复制模式中使用 hjkl 进行光标导航，
 # 而在非复制模式下使用 Ctrl-b + hjkl 在窗格间切换
 
+# 开启24位真色彩
+# 这里的xterm是来源于 echo \$TERM 显示的 xterm-256color
+# 你应当在外部运行 echo \$TERM 来查看你的终端类型，否则不会显示24-bits真色彩
+# 具体见https://stackoverflow.com/questions/41783367/tmux-tmux-true-color-is-not-working-properly
+# set -as terminal-features ",xterm*:RGB"
 EOF
+    if [ -n "$TMUX" ]; then
+        cerror "请在tmux外部运行 echo \$TERM 来查看你的终端类型"
+        cerror "再手动修改~/.tmux.conf，否则不会显示24-bits真色彩"
+    else
+        {
+            echo 'set -as terminal-overrides ",'"$(echo ${TERM%%-*})"'*:Tc"'
+        } >> $TEMP/./.tmux.conf
+    fi
 return 0
 }
